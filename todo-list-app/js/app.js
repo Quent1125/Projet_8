300f4d906efa8dd5715cfb087d3a6b965acba52d
Quentin <quentin.muzel@etu.univ-lyon1.fr>
2020-11-05 17:12:17 +0100

    premiere lecture

--- todo-list-app/js/app.js
+++ todo-list-app/js/app.js
@@ -8,14 +8,14 @@
 	 * @param {string} name The name of your new to do list.
 	 */
 	function Todo(name) {
-		this.storage = new app.Store(name);
+		this.storage = new app.Store(name);     //manque le callback ?
 		this.model = new app.Model(this.storage);
 		this.template = new app.Template();
 		this.view = new app.View(this.template);
 		this.controller = new app.Controller(this.model, this.view);
 	}
 
-	var todo = new Todo('todos-vanillajs');
+	let todo = new Todo('todos-vanillajs');
 
 	function setView() {
 		todo.controller.setView(document.location.hash);
